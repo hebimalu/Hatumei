61faa6af9beedf7b4712b62199089cab5c2bdf7e
hebimalu <126873451+hebimalu@users.noreply.github.com>
2023-06-11 11:09:12 +0900

    Add files via upload

---
+++ Hatumei1/baba/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $p1= $_GET['p1'];
+        $p2= $_GET['p2'];
+
+        echo'<p>おはようございます</p>';
+
+        if($p1=="D"){
+            echo'<p>困ったときは核兵器があるさ</p>';
+        }
+        
+        if($p1=="D"){
+            echo'<p>困ったときは核兵器があるさ</p>';
+        }
+
+    ?>
+</body>
+</html>
